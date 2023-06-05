13fca03609587e4696bf1bf204e998cfa87720e9
bm20019 <bm20019@ues.edu.sv>
2023-06-04 21:11:21 -0600

    Add usuario CRUD, REST API

--- src/main/java/com/portal/comercio/Controllers/usuarioController.java
+++ src/main/java/com/portal/comercio/Controllers/usuarioController.java
@@ -1,17 +1,33 @@
 package com.portal.comercio.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.portal.comercio.Models.usuarios;
+import com.portal.comercio.Models.ventas;
+import com.portal.comercio.Services.usuariosServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import com.portal.comercio.dto.responseDto;
 
 @RestController
+@RequestMapping("comercio/usuarios")
 public class usuarioController {
-
-    @RequestMapping(method = RequestMethod.GET,  value = "comercio/prueba")
-    public String prueba(){
-        return "Hola mundo";
+    @Autowired
+    usuariosServices usuariosSrvc;
+    @RequestMapping(method = RequestMethod.POST, value = "/save")
+    public responseDto saveUsuario(@RequestBody usuarios usuario){
+        return usuariosSrvc.saveUsuarios(usuario);
     }
-    
+    @RequestMapping(method = RequestMethod.GET, value = "/buscar/{id}")
+    public responseDto getUsuarioId(@PathVariable Long id){
+        return usuariosSrvc.getUsuariosId(id);
+    }
+    @RequestMapping(method = RequestMethod.PUT,value = "/update/{id}")
+    public responseDto updateUsuario(@RequestBody usuarios usuario, @PathVariable Long id){
+        return usuariosSrvc.updateUsuarios(usuario, id);
+    }
+    @RequestMapping(method = RequestMethod.PUT,value = "/delete/{id}")
+    public responseDto deleteUsuario(@RequestBody usuarios usuario, @PathVariable Long id){
+        usuarios userTmp =(usuarios)usuariosSrvc.getUsuariosId(id).getRespuesta();
+        return usuariosSrvc.deleteUsuarios(userTmp);
+    }
 }
